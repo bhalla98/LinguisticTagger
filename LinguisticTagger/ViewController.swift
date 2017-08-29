11262b1b2ccc8f5012e17f1bc18c32dbed3ad14f
bhalla98 <siddharthbhalla98@gmail.com>
2017-08-29 23:46:05 +0530

    Initial Commit

--- LinguisticTagger/ViewController.swift
+++ LinguisticTagger/ViewController.swift
@@ -13,13 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tagSchemes = NSLinguisticTagger.availableTagSchemes(forLanguage: "en")
+        let options: NSLinguisticTagger.Options = [.joinNames, .omitWhitespace]
+        let linguisticTagger = NSLinguisticTagger(tagSchemes: tagSchemes, options: Int(options.rawValue))
+        
+        let sentence = "I, funny Siddharth, am playing happily and friendly at school at noon. Wow!"
+        
+        linguisticTagger.string = sentence
+        
+        linguisticTagger.enumerateTags(in: NSMakeRange(0, (sentence as NSString).length), scheme: NSLinguisticTagSchemeNameTypeOrLexicalClass, options: options) { (tag, tokenRange, _, _) in
+            
+            let token = (sentence as NSString).substring(with: tokenRange)
+            print("\(token) -> \(tag)")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
 
