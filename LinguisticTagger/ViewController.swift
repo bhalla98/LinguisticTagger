dbbe27cb38b8749ad55d75fee8ae56aa606bb1d1
bhalla98 <siddharthbhalla98@gmail.com>
2017-08-29 23:26:31 +0530

    Initial Commit

---
+++ LinguisticTagger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LinguisticTagger
+//
+//  Created by siddharth bhalla on 8/29/17.
+//  Copyright © 2017 sb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
